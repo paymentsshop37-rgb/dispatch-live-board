8e8d0cd4a749b1a6564c48a43468e86f7daa9eee
paymentsshop37-rgb <paymentsshop37@gmail.com>
2026-06-20 17:20:12 -0600

    Update DispatchLiveUpdatesPage.jsx

--- src/DispatchLiveUpdatesPage.jsx
+++ src/DispatchLiveUpdatesPage.jsx
@@ -305,6 +305,7 @@ export default function DispatchLiveUpdatesPage() {
   const [currentUserName, setCurrentUserName] = useState(
   localStorage.getItem("currentUserName") || ""
 );
+  }
 
   const isAdmin = currentUserRole === "Admin";
 
